6a263d5a937158ce7cecef3bd2a1b1baef1b4d95
AlexBilka <aleksandrbelka@gmail.com>
2024-01-23 13:16:54 +0200

    Update objects-metods.js

--- objects-metods.js
+++ objects-metods.js
@@ -17,6 +17,7 @@ addPotion(potionName) — метод, який повертає рядок "Addi
 Виклик методу atTheOldToad.addPotion("Power potion") повертає рядок "Adding Power potion"
 */
 
+/*
 const atTheOldToad = {
   potions: [],
   getPotions() {
@@ -26,3 +27,48 @@ const atTheOldToad = {
 		return `Adding ${potions.potionName}`;
   },
 }
+
+*/
+
+/*
+Замовниця хоче, щоб кожне зілля було представлено не тільки ім'ям, але й ціною. Тому зараз у властивості potions буде зберігатися масив об'єктів з властивостями name та price.
+
+Об'єкт atTheOldToad має наступні властивості:
+
+potions — масив рядків, з назвами зілль
+getPotions() — метод, який повертає значення властивості potions
+addPotion() — метод, який приймає параметр об'єкт нового зілля newPotion і додає його в кінець масиву у властивості potions.
+Додай метод getTotalPrice(), який має повертати загальну вартість усіх зілль з властивості potions.
+
+Оголошена змінна atTheOldToad
+Значення змінної atTheOldToad - це об'єкт
+Значення властивості atTheOldToad.getPotions - це метод об'єкта
+Виклик методу atTheOldToad.getPotions() повертає поточне значення властивості potions
+Значення властивості atTheOldToad.addPotion - це метод об'єкта.
+Після виклику методу atTheOldToad.addPotion({ name: "Invisibility", price: 620 }), в масиві potions останнім елементом буде цей об'єкт
+Після виклику методу atTheOldToad.addPotion({ name: "Power potion", price: 270 }), в масиві potions останнім елементом буде цей об'єкт
+Значення властивості atTheOldToad.getTotalPrice - це метод об'єкта
+Виклик методу atTheOldToad.getTotalPrice() повертає загальну вартість усіх зілль з властивості potions
+*/
+
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Stone skin", price: 520 },
+  ],
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    this.potions.push(newPotion);
+  },
+  getTotalPrice() {
+    let totalPrice = 0;
+    for (let potion of this.potions) {
+      totalPrice += potion.price;
+    }
+    return totalPrice;
+  },
+};
+
